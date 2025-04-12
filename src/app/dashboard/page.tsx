e682155c2dd01374b9a8d790514feae5700569f8
Aditya Shinde <aditya.manoj.shinde@gmail.com>
2025-04-12 20:53:52 +0530

    dashboard edited

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,20 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { Settings } from "lucide-react";
+
 function Dashboard() {
-  return <div>page</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 font-bold text-3xl">
+      <Avatar>
+        <AvatarImage src="/logo.svg" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>{" "}
+      Welcome to ColleLink Dashboard
+      <p>
+        <Settings /> Dashboard is still under development...
+      </p>
+    </div>
+  );
 }
 
 export default Dashboard;
