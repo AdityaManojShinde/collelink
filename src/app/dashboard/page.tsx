1d2dda0dc2285c00fa378ece0899e0d6491e6e95
Aditya Shinde <aditya.manoj.shinde@gmail.com>
2025-04-12 21:01:58 +0530

    fixed dashboard temporary ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,14 +5,12 @@ import { Settings } from "lucide-react";
 function Dashboard() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 font-bold text-3xl">
-      <Avatar>
-        <AvatarImage src="/logo.svg" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>{" "}
-      Welcome to ColleLink Dashboard
-      <p>
-        <Settings /> Dashboard is still under development...
-      </p>
+      <div className="shadow-lg p-10 bg-white dark:bg-gray-800 rounded-lg flex flex-col items-center">
+        <h2 className="m-2">
+          <Settings className="inline-block" /> Dashboard is still under
+          development...
+        </h2>
+      </div>
     </div>
   );
 }
